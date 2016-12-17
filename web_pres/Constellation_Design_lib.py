00222eef3da2d8890727463a8b2dcfb1a7dbeb0f
Pavel Prochazka <prochp10@fel.cvut.cz>
2016-12-17 23:30:16 +0100

    @nd section done + basic web interface

--- web_pres/Constellation_Design_lib.py
+++ web_pres/Constellation_Design_lib.py
@@ -1,4 +1,7 @@
 import numpy as np
+from matplotlib.patches import Polygon, Circle
+from matplotlib.pyplot import cm
+import matplotlib.pyplot as plt
 
 
 def find_levels(i, levels, const, const_el):
@@ -36,6 +39,17 @@ def const_design_XOR(Nb, Ns, h = 1.): # Relative Channel Parameterization
     relay_const = np.asarray([x + y * h for x in sourceA_const for y in sourceB_const])
     return (sourceA_const, sourceB_const, basic_part, superposed_part, relay_const, alpha)
 
+def QAM(N): # QAM constellation
+    odd = N % 2
+    base = (N - odd) / 2
+    min_Re = -2**(base+odd) + 1
+    max_Re = 2**(base+odd) - 1
+    min_Im = -2**base + 1
+    max_Im = 2**base - 1
+    const = np.asarray([i + 1j*j  for i in np.arange(min_Re, max_Re+1, 2)\
+                                     for j in np.arange(min_Im, max_Im+1, 2)])
+    scale = np.sqrt((np.abs(const)**2).sum()/float(2**N))
+    return const / scale, 1./scale
 
 
 #plt.plot(np.real(relay_const), np.imag(relay_const), 'ok', ms=10)
@@ -58,23 +72,29 @@ def intersect(lin, lin_vec):
 def prepare_lines(Points):
     '''
     Evaluate lines perpendicular to the vector given by individual points
+    line is given by ax + by + c = 0
     '''
     lins = []
     for p in Points:
-        px = np.real(p)+np.imag(p)**2/np.real(p) # cross x axis
-        py = np.abs(p)/np.sin(np.angle(p))*1j # cross y axis
-        c = np.imag(py)
-        a = -c/np.real(px)
-        b = -1
         if np.abs(np.imag(p)) < 1e-10:
             py = np.inf # no crossing y (imag) axis        
+            px = np.real(p)+np.imag(p)**2/np.real(p) # cross x axis
             c = px
             a = -1
             b = 0        
-        if np.abs(np.real(p)) < 1e-10:
+        elif np.abs(np.real(p)) < 1e-10:
             px = np.inf # no crossing x (real) axis     
+            py = np.abs(p)/np.sin(np.angle(p))*1j # cross y axis
             a = 0  
+            b = -1
             c = float(np.imag(py))
+        else:
+            px = np.real(p)+np.imag(p)**2/np.real(p) # cross x axis
+            py = np.abs(p)/np.sin(np.angle(p))*1j # cross y axis
+            c = np.imag(py)
+            b = -1
+            a = -c/np.real(px)
+            
         lins.append([a,b,c])
     return np.asarray(lins)
 
@@ -112,17 +132,50 @@ def unique_complex(vec, eq = 1e-5):
     unique the vector, where equality means np.abs(a-b)<eq
     '''
     uniques_vec = [vec[0]]
-    for c in vec[1:]:
+    mapping = [0]
+    for i, c in enumerate(vec[1:]):
         if np.min(np.abs(c-np.asarray(uniques_vec))) > eq:
-            uniques_vec.append(c)
-    return np.asarray(uniques_vec)
+            uniques_vec.append(c) 
+            mapping.append(i+1)
+    return np.asarray(uniques_vec), np.asarray(mapping)
+
+def check_excl_failures(relay_const, Nb, Ns, eq = 1e-5):
+    '''
+    unique the vector, where equality means np.abs(a-b)<eq and check for the exlusive law failures
+    '''
+    uniques_vec = []
+    mapping = []
+    J2H = np.zeros(4**(Nb+Ns))
+    C2H = [] # mapped symbols to hier symbol
+    C2B = [] # mapped symbols to basic part
+    Failures = []    
+    for dAb in range(2**Nb):
+        for dBb in range(2**Nb):
+            for dAs in range(2**Ns):
+                for dBs in range(2**Ns):
+                    ind = dAs*2**(Ns+2*Nb) + dAb*2**(Ns+Nb)+dBs*2**(Nb) + dBb
+                    ind_b = dAb ^ dBb
+                    indH = dAs*2**(Ns+Nb) + dBs*2**(Nb) + ind_b
+                    c = relay_const[ind]
+                    if (len(uniques_vec) == 0) or (np.min(np.abs(c-np.asarray(uniques_vec))) > eq):
+                        uniques_vec.append(c)
+                        mapping.append(J2H)
+                        C2H.append(indH)
+                        C2B.append(ind_b)
+                        Failures.append(False)                        
+                    else:
+                        for pos in np.flatnonzero(np.abs(c-np.asarray(uniques_vec)) <= eq):
+                            if C2H[pos] != indH:
+                                Failures[pos] = True
+    return C2H, C2B, np.asarray(uniques_vec), Failures
+    
 
 def Get_Dec_Region_Boundaries(const, mmax = 3):
     '''
     Return list of borders defining polygons for decision region
     '''
     bord = []
-    unique_const = unique_complex(const)
+    unique_const, mapping = unique_complex(const)
     for ind, c in enumerate(unique_const):        
         bord_frame = np.array([mmax - np.real(c),-mmax - np.real(c), 1j*mmax - 1j*np.imag(c),-1j*mmax - 1j*np.imag(c)])
         _Points = (np.hstack([unique_const[:ind], unique_const[(ind+1):]]) - c)*0.5
@@ -131,169 +184,232 @@ def Get_Dec_Region_Boundaries(const, mmax = 3):
         bord.append(find_border_points(lins, Points) + c)
     return bord
     
-def Draw_dec_Boundaries(ax, bords, col):
+def Draw_dec_Boundaries(ax, bords, hatches, mapping = None, Failures = None):
     '''
     Draw the boundaries
-    '''        
+    '''       
     for i,  bord in enumerate(bords):
-        ax.plot(np.real(bord), np.imag(bord),'k-', lw=2)    
-        #xy=np.vstack([np.real(bord), np.imag(bord)]).T
-        #bar = plt.Polygon(xy, color=col[i], ec='k',lw=2, label=str(i))
-        #ax.add_artist(bar)        
+        #ax.plot(np.real(bord), np.imag(bord),'k-', lw=2)   
+        if mapping is not None:
+            xy=np.vstack([np.real(bord), np.imag(bord)]).T
+            if not Failures[i]:
+                bar = plt.Polygon(xy, ec='k',lw=2, hatch=hatches[mapping[i]], facecolor='white')
+            else:
+                bar = plt.Polygon(xy, ec='k',lw=2, facecolor='red', fill=True, alpha=0.4)
+            ax.add_artist(bar)      
+        else:
+            bar = plt.Polygon(xy, color=col[i], ec='k',lw=2, label=str(i))
     #ax.plot(np.real(const), np.imag(const),'ok',ms=8)
     
+def Draw_Error_Circles(const, sigma2w, Pe):
+    '''
+    Draw the circles for the given constellation
+    '''
+    N = 100000
+    Pe = 0.1
+    x  = np.sqrt(sigma2w/2)*(np.random.randn(N)+np.random.randn(N)*1j)
+    d = np.sqrt(-sigma2w*np.log(Pe))
 
-def Draw_Relay_Const(ax, Nb, Ns, h):
+
+
+
+def Draw_Relay_Const(ax, Nb, Ns, h, mmax = 3):
     (sourceA_const, sourceB_const, basic_part, superposed_part, relay_const, alpha) = const_design_XOR(Nb, Ns, h)    
-    #markers_SRC = [(2+i, 1 , 0) for i in range(2**Nb)]
-    markers_R = [(2+i, 2 , 0) for i in range(2**Nb)]
-    col = cm.rainbow(np.linspace(0,1,2**(2*Ns)))
+    col = cm.rainbow(np.linspace(0,1,2**Ns))
+    base_hatch_Relay = ['o', 'O','.','*', '\\']
+    hatches_Relay = [''.join([base_hatch_Relay[int(i)] for i in np.base_repr(ind,len(base_hatch_Relay))]) for ind in range(2**Nb)]
+    C2H, C2B, unique_const, Failures = check_excl_failures(relay_const, Nb, Ns, eq = 1e-5)
+    bords = Get_Dec_Region_Boundaries(unique_const, mmax) #prepare decision regions for relay constellation
+    Draw_dec_Boundaries(ax, bords, hatches_Relay, C2B, Failures)    
+    
+    col = cm.rainbow(np.linspace(0,1,2**Ns)) 
     for dAb in range(2**Nb):
         for dBb in range(2**Nb):
             for dAs in range(2**Ns):
-                for dBs in range(2**Ns):
-                    ind_s = dAs * 2**Ns + dBs
-                    ind_b = dAb ^ dBb
+                for dBs in range(2**Ns):                                        
                     c = basic_part[dAb] + h*basic_part[dBb] + superposed_part[dAs] + h*1j*superposed_part[dBs]
-                    ax.plot(np.real(c), np.imag(c), marker=markers_R[ind_b], color=col[ind_s], mew=3, ms=16)
+                    ax.plot(np.real(c), np.imag(c), 'o', color=col[dAs], mew=3, ms=16, fillstyle='left')
+                    ax.plot(np.real(c), np.imag(c), 'o', color=col[dBs], mew=3, ms=16, fillstyle='right')                    
+    ax.grid()    
+    ax.set_xlim([-3,3])
+    ax.set_ylim([-3,3])
     
-def Draw_SRC_Const(ax, Nb, Ns, node == 'A'):
-    (sourceA_const, sourceB_const, basic_part, superposed_part, relay_const, alpha) = const_design_XOR(Nb, Ns, h)    
-    markers_SRC = [(2+i, 1 , 0) for i in range(2**Nb)]
-    #markers_R = [(2+i, 2 , 0) for i in range(2**Nb)]
-    col = cm.rainbow(np.linspace(0,1,2**(2*Ns)))
+def Draw_SRC_Const(ax, Nb, Ns, node = 'A'):
+    (sourceA_const, sourceB_const, basic_part, superposed_part, relay_const, alpha) =    const_design_XOR(Nb, Ns, 1.)    
+    col = cm.rainbow(np.linspace(0,1,2**Ns))
+    base_hatch_SRC = ['/','-', '+', 'x']
+    hatches_SRC = [''.join([base_hatch_SRC[int(i)] for i in np.base_repr(ind,len(base_hatch_SRC))]) for ind in range(2**Nb)]
+    ax.axis('equal')
+ 
+    
     for db in range(2**Nb):
-        for ds in range(2**Nb):
-            ind_s = dAs * 2**Ns + dBs
-            ind_b = dAb ^ dBb
-            c = basic_part[dAb] + h*basic_part[dBb] + superposed_part[dAs] + h*1j*superposed_part[dBs]
-            ax.plot(np.real(c), np.imag(c), marker=markers_R[ind_b], color=col[ind_s], mew=3, ms=16)
+        for ds in range(2**Ns):
+            sA = basic_part[db] + superposed_part[ds]
+            sB = basic_part[db] + 1j*superposed_part[ds]
+            if node == 'A':                               
+                ax.plot(np.real(sA), np.imag(sA), 'o', color=col[ds], mew=3, ms=16, fillstyle='left')                                
+                ax.scatter(np.real(sA), np.imag(sA), s=1000, marker='s', hatch=hatches_SRC[db], facecolor='white')                
+            else:
+                ax.plot(np.real(sB), np.imag(sB), 'o', color=col[ds], mew=3, ms=16, fillstyle='right')
+                ax.scatter(np.real(sB), np.imag(sB), s=1000, marker='s', hatch=hatches_SRC[db], facecolor='white')                
+    ax.grid()
+    ax.set_xlim([-1.5, 1.5])
+    ax.set_ylim([-1.5, 1.5])
+
+def insert_legend(Nb, Ns, ax):
+    col = cm.rainbow(np.linspace(0,1,2**Ns))
+    base_hatch_SRC = ['/','-', '+', 'x']
+    base_hatch_Relay = ['o', 'O','.','*', '\\']
+    hatches_SRC = [''.join([base_hatch_SRC[int(i)] for i in np.base_repr(ind,len(base_hatch_SRC))]) for ind in range(2**Nb)]
+    hatches_Relay = [''.join([base_hatch_Relay[int(i)] for i in np.base_repr(ind,len(base_hatch_Relay))]) for ind in range(2**Nb)]
+
+    for i in range(2**Ns):
+        Bleg, = ax.plot(-100,-100,'o', color=col[i], mew=3, ms=16, fillstyle='right', label='$d_B^s$=%d'%i)
+        Aleg, = ax.plot(-100,-100,'o', color=col[i], mew=3, ms=16, fillstyle='left', label='$d_A^s$=%d'%i)
+    [ax.scatter(-100, -100 , s=500, marker='s', facecolor='white', hatch=hatches_SRC[i], label=r'$d^b_S$=%d'%i) for i in range(2**Nb)]
+    [ax.scatter(-100, -100 , s=500, marker='s', facecolor='white', hatch=hatches_Relay[i], label='$d^b_R$=%d'%i) for i in range(2**Nb)]
+    ax.set_xlim([0,1])
+    ax.set_ylim([0,1])
+    ax.legend(scatterpoints=1, numpoints=1,ncol=2)
+    ax.axis('off')
 
     
 #%%
-from matplotlib.patches import Polygon, Circle
-from matplotlib.pyplot import cm
-    
-Nb = 2
-Ns = 2
-h  = 1.
-mmax = 3
 
-(sourceA_const, sourceB_const, basic_part, superposed_part, relay_const, alpha) = const_design_XOR(Nb, Ns, h)
-col = cm.rainbow(np.linspace(0,1,len(const)))    
-bords = Get_Dec_Region_Boundaries(relay_const, mmax)
+if __name__ == '__main__':
+    Nb = 2
+    Ns = 0
+    h  = 0.96+0.12j
+    mmax = 3
 
-fig, ax = plt.subplots()
-Draw_Relay_Const(ax, Nb, Ns, h)    
-Draw_dec_Boundaries(ax, bords, col)
+    (sourceA_const, sourceB_const, basic_part, superposed_part, relay_const, alpha) = const_design_XOR(Nb, Ns, h)
+
+    col = cm.rainbow(np.linspace(0,1,len(relay_const))) 
+    base_hatch = ['/','-', '+', 'x', 'o', 'O','.','*', '\\']
+    hatches = [''.join([base_hatch[int(i)] for i in np.base_repr(ind,len(base_hatch))]) for ind in range(2**Nb)]
+#hatches = [''.join([b for i in range(j)]) for b in base_hatch for j in range(2**Nb)]
+               
+       
+
+
+    fig = plt.figure(figsize=(15,10))
+    ax_dA = plt.subplot2grid((2,3), (0, 0));ax_dA.axis('equal')
+    ax_dB = plt.subplot2grid((2,3), (1, 0));ax_dB.axis('equal')
+    ax_R = plt.subplot2grid((2,3), (0, 1), colspan=2, rowspan=2)
+
+    Draw_SRC_Const(ax_dA, Nb, Ns, node = 'A')
+    Draw_SRC_Const(ax_dB, Nb, Ns, node = 'B')
+
+    Draw_Relay_Const(ax_R, Nb, Ns, h)    
+
 
 
 
 
 #%% DEBUG
-
-def Merge_2Borders(bord1, bord2):
-    '''
-    Merge selected 2 boundaries (if possible)
-    '''
-    inter =  np.intersect1d(bord1, bord2)    
-    if len(inter) == 2:        
-        ind1 = [np.flatnonzero(bord1 == i) for i in inter]
-        ind2 = [np.flatnonzero(bord2 == i) for i in inter]
-        res[0] = inter[0]
-        act_ind = ind1[0]      
-    else:   
-        return [bord1, bord2]
-
-#%%
-import sympy as sp
-
-
-a,b,c,a1,b1,c1,x0,y0,x1,y2 = sp.symbols('a,b,c,a1,b1,c1,x0,y0,x1,y2')
-sp.solve([a*x0+b*y0+c, a1*x0+b1*y0+c1], x0, y0)
-
-
-
-#%%
-ind = 0
-act_point = relay_const[ind]
-cc = np.hstack([relay_const[:ind],relay_const[(ind+1):]]) - act_point
-plt.plot(np.real(cc), np.imag(cc), 'ok', ms=10)
-plt.plot(0,0, 'og', ms=10)
-
-points = 0.5 * np.hstack([cc[:ind],cc[(ind+1):]]) # mid  points
-
-
-plt.plot(np.real(points), np.imag(points), 'xg', ms=5)
-
-#%% preparation of lines
-plt.figure(figsize(12,12))
-c1=[]
-c2=[]
-mp = []
-lins = []
-N = 0
-plt.plot(0,0,'or', ms=15)
-Points = np.hstack([-1.5,1.5,-1.5j,1.5j, np.exp(1j*np.random.rand(N)*2*np.pi)*(1+0.1*np.random.rand(N))])
-
-for p in Points:
-    px = np.real(p)+np.imag(p)**2/np.real(p) # cross x axis
-    py = np.abs(p)/np.sin(np.angle(p))*1j # cross y axis
-    c = np.imag(py)
-    a = -c/np.real(px)
-    b = -1
-    if np.abs(np.imag(p)) < 1e-10:
-        py = np.inf # no crossing y (imag) axis        
-        c = px
-        a = -1
-        b = 0        
-    if np.abs(np.real(p)) < 1e-10:
-        px = np.inf # no crossing x (real) axis     
-        a = 0  
-        c = float(np.imag(py))
-#    # check
-#    c1.append(np.abs(px)**2 - np.abs(p-px)**2 - np.abs(p)**2)
-#    c2.append(np.abs(p)**2+np.abs(py-p)**2-np.abs(py)**2)
-    lins.append([a,b,c])
-    plt.plot(np.real(p), np.imag(p), 'og', ms=5)
-    ran = np.array([-2,2])
-    if b == 0:
-        #plt.plot([c, c],ran, '--k')
-        mp.append(np.array([c, c])+ran*1j)
-    else:
-        #plt.plot(ran,a*ran+c, '--k')
-        mp.append(ran+(a*ran+c)*1j)
-
-lins = np.asarray(lins)
-   
-for i in mp:
-    plt.plot(np.real(i), np.imag(i), '--k')
-    
-
-selected = []
-sel_intersect = []
-bord_lines = []
-
-ind_sel = np.argmin(np.abs(Points)) # the first selected point (index)
-selected.append(ind_sel)
-
-while True:
-    point_sel = Points[ind_sel] 
-    lin_sel = lins[ind_sel]
-    
-    P_rem = Points * np.exp(-1j*np.angle(point_sel)) # rotation that selected point has angle zero    
-    sat_inds, = np.nonzero((np.mod(np.angle(P_rem), 2*np.pi) > 1e-10)*(np.mod(np.angle(P_rem), 2*np.pi) < np.pi - 1e-10)) # positive indecis    
-    intersects = intersect(lin_sel, lins[sat_inds])    
-    print point_sel, intersects, sat_inds
-    dists = np.abs(intersects - point_sel)
-    ind_min = np.argmin(dists)
-    sel_intersect.append(intersects[ind_min])
-    ind_sel = sat_inds[ind_min]
-    if ind_sel == selected[0]:
-        break
-    else:
-        selected.append(ind_sel)       
-borders = np.hstack([sel_intersect, sel_intersect[0]])
-
-
-plt.plot(np.real(borders), np.imag(borders), 'k-', lw=2)
+#
+#def Merge_2Borders(bord1, bord2):
+#    '''
+#    Merge selected 2 boundaries (if possible)
+#    '''
+#    inter =  np.intersect1d(bord1, bord2)    
+#    if len(inter) == 2:        
+#        ind1 = [np.flatnonzero(bord1 == i) for i in inter]
+#        ind2 = [np.flatnonzero(bord2 == i) for i in inter]
+#        res[0] = inter[0]
+#        act_ind = ind1[0]      
+#    else:   
+#        return [bord1, bord2]
+#
+##%%
+#import sympy as sp
+#
+#
+#a,b,c,a1,b1,c1,x0,y0,x1,y2 = sp.symbols('a,b,c,a1,b1,c1,x0,y0,x1,y2')
+#sp.solve([a*x0+b*y0+c, a1*x0+b1*y0+c1], x0, y0)
+#
+#
+#
+##%%
+#ind = 0
+#act_point = relay_const[ind]
+#cc = np.hstack([relay_const[:ind],relay_const[(ind+1):]]) - act_point
+#plt.plot(np.real(cc), np.imag(cc), 'ok', ms=10)
+#plt.plot(0,0, 'og', ms=10)
+#
+#points = 0.5 * np.hstack([cc[:ind],cc[(ind+1):]]) # mid  points
+#
+#
+#plt.plot(np.real(points), np.imag(points), 'xg', ms=5)
+#
+##%% preparation of lines
+#plt.figure(figsize(12,12))
+#c1=[]
+#c2=[]
+#mp = []
+#lins = []
+#N = 0
+#plt.plot(0,0,'or', ms=15)
+#Points = np.hstack([-1.5,1.5,-1.5j,1.5j, np.exp(1j*np.random.rand(N)*2*np.pi)*(1+0.1*np.random.rand(N))])
+#
+#for p in Points:
+#    px = np.real(p)+np.imag(p)**2/np.real(p) # cross x axis
+#    py = np.abs(p)/np.sin(np.angle(p))*1j # cross y axis
+#    c = np.imag(py)
+#    a = -c/np.real(px)
+#    b = -1
+#    if np.abs(np.imag(p)) < 1e-10:
+#        py = np.inf # no crossing y (imag) axis        
+#        c = px
+#        a = -1
+#        b = 0        
+#    if np.abs(np.real(p)) < 1e-10:
+#        px = np.inf # no crossing x (real) axis     
+#        a = 0  
+#        c = float(np.imag(py))
+##    # check
+##    c1.append(np.abs(px)**2 - np.abs(p-px)**2 - np.abs(p)**2)
+##    c2.append(np.abs(p)**2+np.abs(py-p)**2-np.abs(py)**2)
+#    lins.append([a,b,c])
+#    plt.plot(np.real(p), np.imag(p), 'og', ms=5)
+#    ran = np.array([-2,2])
+#    if b == 0:
+#        #plt.plot([c, c],ran, '--k')
+#        mp.append(np.array([c, c])+ran*1j)
+#    else:
+#        #plt.plot(ran,a*ran+c, '--k')
+#        mp.append(ran+(a*ran+c)*1j)
+#
+#lins = np.asarray(lins)
+#   
+#for i in mp:
+#    plt.plot(np.real(i), np.imag(i), '--k')
+#    
+#
+#selected = []
+#sel_intersect = []
+#bord_lines = []
+#
+#ind_sel = np.argmin(np.abs(Points)) # the first selected point (index)
+#selected.append(ind_sel)
+#
+#while True:
+#    point_sel = Points[ind_sel] 
+#    lin_sel = lins[ind_sel]
+#    
+#    P_rem = Points * np.exp(-1j*np.angle(point_sel)) # rotation that selected point has angle zero    
+#    sat_inds, = np.nonzero((np.mod(np.angle(P_rem), 2*np.pi) > 1e-10)*(np.mod(np.angle(P_rem), 2*np.pi) < np.pi - 1e-10)) # positive indecis    
+#    intersects = intersect(lin_sel, lins[sat_inds])    
+#    print point_sel, intersects, sat_inds
+#    dists = np.abs(intersects - point_sel)
+#    ind_min = np.argmin(dists)
+#    sel_intersect.append(intersects[ind_min])
+#    ind_sel = sat_inds[ind_min]
+#    if ind_sel == selected[0]:
+#        break
+#    else:
+#        selected.append(ind_sel)       
+#borders = np.hstack([sel_intersect, sel_intersect[0]])
+#
+#
+#plt.plot(np.real(borders), np.imag(borders), 'k-', lw=2)
---
+++ web_pres/index.php
@@ -0,0 +1,10 @@
+<html>
+<h1>WNC Tutorial - Constellation Design for Butterfly Network</h1>
+<br>
+<p>
+<h3>Sections:</h3>
+<ol>
+<li><a href='Network_Information_Flow.html'>Network Information Flow</a></li>
+<li><a href='From_NIF_to_WNC.html'>From Network Information Flow to Wireless Network Coding</a></li>
+</ol>
+</html>
